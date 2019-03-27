a3841ae725fa350af91d48bede981c9f0565841d
rmitleon <48973802+rmitleon@users.noreply.github.com>
2019-03-27 15:29:09 +1100

    Update Menu.java

--- AssSprint2/src/Menu.java
+++ AssSprint2/src/Menu.java
@@ -8,7 +8,7 @@ public class Menu {
   System.out.println(" Please select from the following option");
   System.out.println("===================================");
   System.out.println("1. Make a booking");
-  System.out.println("2. List all sessions");
+  ("2. List all sessions");
   System.out.println("3. Find a movie session by name");
   System.out.println("4. Delete a booking");
   System.out.println("5. List all bookings");
@@ -18,9 +18,9 @@ public class Menu {
  }
  
  //Add menu
- public void locationMenu () {
+ cationMenu () {
   System.out.println("Select cinema theater location from the folowing options");
-  System.out.println("===================================");
+  SysSystem.out.printlntem.out.println("===================================");
   System.out.println("1.St Kilda");
   System.out.println("2.Fitzroy");
   System.out.println("3.Melbourne CBD");
